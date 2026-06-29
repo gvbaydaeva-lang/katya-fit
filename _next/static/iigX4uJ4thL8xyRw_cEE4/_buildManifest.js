e9d558601ea718f4f9aba79d84c257e9651657b9
gvbaydaeva-lang <gvbaydaeva-lang@users.noreply.github.com>
2026-06-29 10:31:25 +0000

    deploy: cf4027ec70df7de5bb36364103073839101ae6fe

---
+++ _next/static/iigX4uJ4thL8xyRw_cEE4/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/katya-fit//_next/:path+",
+        "destination": "/katya-fit/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
